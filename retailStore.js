b1bbc003cd9f5af21919bb4eda3edfc134cc1677
katie hoffman <kathryn.hoffman@detroitlabs.com>
2017-03-22 14:57:13 -0400

    Adding comments for scope of assignment

---
+++ retailStore.js
@@ -0,0 +1,19 @@
+/*
+
+- Create an inventory application for a retail store
+- Each item in the inventory should be represented by an object, which should store a description of the item, the price of the item, and the number of items in stock
+- Users should be able to add an item, delete an item, search for an item, modify the number of items available, and change an item’s description
+
+*/
+
+// Constructor of Item (description, price, inventory number)
+
+// Add an item
+
+// Delete Item
+
+// Search for an Item
+
+// Modify the number of items available
+
+// Change item description
